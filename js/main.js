1f28a6410eaa72cf054b365e947a1976e422659c
yuanjli <liyuanjienetwork@gmail.com>
2018-08-09 11:29:43 -0700

    added character and movement functionality

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 //https://www.youtube.com/watch?v=b6A4XHkTjs8
 
 
+
+
 console.log('JavaScript for ac is loading!');
 
 var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'game', {
@@ -17,7 +19,7 @@ var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'game', {
 // ========== variables ===========
 var player, cursors;
 
-var GAME_HEIGHT = 540;
+var GAME_HEIGHT = 460;
 var GAME_WIDTH = 960;
 
 var pPosition;
@@ -32,35 +34,57 @@ function init() {
 
 function preload() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-
+	//preload the image files: 
 	game.load.image('bg', '../assets/img/cave_bg.png');
 	game.load.image('person', '../assets/img/down_char.png');
+
+	//load audio files for later uses:
+	game.load.audio('music', '../assets/audio/among-the-clouds.mp3');
+	game.load.audio('die', '../assets/audio/die.wav');
+	game.load.audio('hit', '../assets/audio/hit.wav');
+	game.load.audio('point', '../assets/audio/point.wav');
+	game.load.audio('swoosh', '../assets/audio/swoosh.wav');
+	game.load.audio('wing', '../assets/audio/wing.wav');
 
 }
 
 function create(){
+
+	// this sets up the background image and auto slides to the left:
 	var background = game.add.tileSprite(0, 0, game.width, game.height, 'bg');
 	background.autoScroll(-30, 0);
 
+	// set up the music sound: 
+	music = game.add.audio('music');
+	music.play();
 
+	//creates the person charactor in the window and added gravity
 	person = game.add.sprite(260, 200, 'person');
 	game.physics.arcade.enable(person);
+	person.score = 0;
 	person.body.collideWorldBounds = true;
 	person.body.gravity.y=800;
 
+	//needs to create pipes on the window
+
+	// need those two lines to stop the browser functions interference with the game.
+	cursors = game.input.keyboard.createCursorKeys();
+	game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.ENTER]);
 
 
-
-
+	//Add Score and HP text to the screen: 
+	//hpText = game.add.text(GAME_WIDTH - 150, 20, 'Life: ' + player.life.toString(), {fill: '#fff'});
+	scoreText = game.add.text(GAME_WIDTH - 150, GAME_HEIGHT - 45, 'Score: ' + person.score.toString(), {fill: '#fff'});
 
 }
 
 function update(){
 	if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
 		// the person is going up :
-		abc();
+		person.body.velocity.y = -200;
 	}
 	else if (pVelocity>0) {
+
 
 	}
 		// the person is going down
