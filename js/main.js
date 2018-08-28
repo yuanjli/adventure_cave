f82f282b6a79993b56920bf093449ac88fcad7dd
yuanjli <liyuanjienetwork@gmail.com>
2018-08-27 18:07:00 -0700

    updated scoring and gameover functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// 
-// The adventure cave game is based on recreating flappy bird.
-// toturials:  
-// https://www.youtube.com/watch?v=b6A4XHkTjs8
-// https://phaser.io/news/2015/03/flappy-bird-tutorial
 
 // ========== variables =======
 // Always define global varriables before functions and calls;
@@ -17,12 +12,9 @@ var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'game', {
 	create: create,
 	update: update
 });
-<<<<<<< HEAD
-//  ============================
-=======
+
 
 //  ==================================
->>>>>>> gh-pages
 // Initialize the game;
 function init() {
 	console.log('init');
@@ -33,23 +25,7 @@ function preload() {
 	// Initialize arcade physics:
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-<<<<<<< HEAD
-	//preload the image files: 
-	game.load.image('bg', '../assets/img/cave_bg.png');
-	game.load.image('person', '../assets/img/down_char.png');
-	game.load.image('pipeUp', '../assets/img/red_pipe.png');
-	game.load.image('pipeDown', '../assets/img/red_pipe_down.png');
-	game.load.image('floor', '../assets/img/floor_bg.png');
-	//load animations
-	game.load.spritesheet('flap', '../assets/img/flap_character.png', 70, 43);
-	//load audio files for later uses:
-	game.load.audio('music', '../assets/audio/among-the-clouds.mp3');
-	game.load.audio('die', '../assets/audio/die.wav');
-	game.load.audio('hit', '../assets/audio/hit.wav');
-	game.load.audio('point', '../assets/audio/point.wav');
-	game.load.audio('swoosh', '../assets/audio/swoosh.wav');
-	game.load.audio('wing', '../assets/audio/wing.wav');
-=======
+
 	// Preload the image files: 
 	game.load.image('bg', './assets/img/cave_bg.png');
 	game.load.image('person', './assets/img/down_char.png');
@@ -67,7 +43,6 @@ function preload() {
 	game.load.audio('point', './assets/audio/point.wav');
 	game.load.audio('swoosh', './assets/audio/swoosh.wav');
 	game.load.audio('wing', './assets/audio/wing.wav');
->>>>>>> gh-pages
 }
 
 // Create the game elements;
@@ -89,16 +64,10 @@ function create(){
 	game.physics.arcade.enable(person);  //enable the person's physics. 
 	person.score = 0;
 	//person.body.collideWorldBounds = true;
-<<<<<<< HEAD
 	person.body.gravity.y = 900;    // adds gravity to the player person.
 	person.animations.add('flap', [0, 1], 20, true); // adds the animation to the person
-	
-=======
-	person.body.gravity.y = 900;
-	person.animations.add('flap', [0, 1], 20, true);
 	person.score = 0;
 	person.topScore = localStorage.getItem('bestScore') == null ? 0 : localStorage.getItem('bestScore');
->>>>>>> gh-pages
 
 	//======< to create pipes as obstacles =======>
 	// create pipe in a group 
@@ -118,44 +87,33 @@ function create(){
 	game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.ENTER]);
 
 	//Add Score and HP text to the screen: 
-<<<<<<< HEAD
-=======
+
 	//hpText = game.add.text(GAME_WIDTH - 130, 20, 'Life: ' + player.life.toString(), {fill: '#fff'});
 	topScoreText = game.add.text(20, 20, 'Top Score: ' + person.topScore.toString(), {fill: '#fff'});
->>>>>>> gh-pages
 	scoreText = game.add.text(GAME_WIDTH - 135, GAME_HEIGHT - 480, 'Score: ' + person.score.toString(), {fill: '#fff'});
 }
 
 // Updates the game logic;
 function update(){
-<<<<<<< HEAD
 	person.play('flap');  //plays the animation on the person;
-=======
 
-	person.play('flap');
->>>>>>> gh-pages
 	if (person.angle < 20) {
 		person.angle += 1;    //create the down motion, which is rotate down. 
 	}
 	if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
 		person.body.velocity.y = -230;    		// the person is going up;
 		game.add.tween(person).to({angle: -20}, 100).start(); 
-<<<<<<< HEAD
+
 		person.anchor.setTo(-0.2, 0.5);   //person rotate upward;
 		swoosh.play(); 
-=======
-		person.anchor.setTo(-0.2, 0.5); 
-		swoosh.play();
+
 		//person.play('flap');
->>>>>>> gh-pages
+
 	}
 	else {
 		person.animations.stop();
 	}
-<<<<<<< HEAD
-	// this function is the collision detection
-	game.physics.arcade.overlap(person, pipes, hitPipe);
-=======
+
 	// Todo: if the person hits the floor, gameOver
 	if (person.y < 0 || person.y > game.height) {
 		hit.play();
@@ -165,7 +123,7 @@ function update(){
 	// console.log(game.physics.arcade.overlap(person, pipes, hitPipe));
 	// console.log('==******************>', game.physics.arcade.overlap(person, pipes, hitPipe));
 	game.physics.arcade.overlap(person, pipes, hitPipe);	
->>>>>>> gh-pages
+
 }
 
 // ===========<pipes> ===============
@@ -217,7 +175,7 @@ function addScore(){
 		setTimeout(function() { person.score += 1 }, 3500);
 	};
 //	person.score += amount;
-	scoreText.text = 'Score: ' + person.score.toString();
+scoreText.text = 'Score: ' + person.score.toString();
 }
 
 
@@ -239,23 +197,23 @@ function gameOver() {
 	game.paused = true;
 	if (person.score >= 100) {
 		swal({
-		title: 'You Win!',
-		text: 'Thanks for playing!',
-		type: 'warning', 
-		showCancel: false,
-		confirmButtonText: 'Awesome',
-		closeOnConfirm: true
-	}); }
-	 else {
-	swal({
-		title: 'Great job!',
-		text: 'Thanks for playing, you can do better!',
-		type: 'warning', 
-		showCancel: false,
-		confirmButtonText: 'Cool',
-		closeOnConfirm: true
-	});
-}
+			title: 'You Win!',
+			text: 'Thanks for playing!',
+			type: 'warning', 
+			showCancel: false,
+			confirmButtonText: 'Awesome',
+			closeOnConfirm: true
+		}); }
+		else {
+			swal({
+				title: 'Great job!',
+				text: 'Thanks for playing, you can do better!',
+				type: 'warning', 
+				showCancel: false,
+				confirmButtonText: 'Cool',
+				closeOnConfirm: true
+			});
+		}
 	//game.time.events.stop();
 	//gameOverText = game.add.text(300, 200, 'Score: ' + player.score.toString(), {font: '45px oblique', fill: '#fff'});
 }
